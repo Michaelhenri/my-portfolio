110f34a4d14dfa9d8f6d3b25395f3023c149c3a3
Michaelhenri <michaeldt2015@gmail.com>
2024-06-24 14:18:34 -0300

    ajustes no tema da página

--- theme.js
+++ theme.js
@@ -2,12 +2,15 @@ const moon = document.querySelector('.icone-lua')
 const sun = document.querySelector('.icone-sol')
 const body = document.querySelector('body');
 const header = document.querySelector('header')
+const footer = document.querySelector('footer')
 
 function mudarParaTemaEscuro() {
   body.classList.remove('light');
   body.classList.add('dark');
   header.classList.remove('light');
   header.classList.add('dark');
+  footer.classList.remove('light');
+  footer.classList.add('dark');
   localStorage.setItem('temaPreferido', 'dark');
 }
 
@@ -16,10 +19,10 @@ function mudarParaTemaClaro() {
   body.classList.add('light');
   header.classList.remove('dark');
   header.classList.add('light');
+  footer.classList.remove('dark');
+  footer.classList.add('light');
   localStorage.setItem('temaPreferido', 'light');
 }
 
 moon.addEventListener('click', mudarParaTemaEscuro);
 sun.addEventListener('click', mudarParaTemaClaro);
-
-
